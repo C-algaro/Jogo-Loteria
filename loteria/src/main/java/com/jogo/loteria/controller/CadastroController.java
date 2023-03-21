cb7f9d40f1e6761271241c6b5245182dbca69c01
C-algaro <ti.chrystian@gmail.com>
2023-03-20 23:48:27 -0300

    Ajustes - Sidebar e Footer

--- loteria/src/main/java/com/jogo/loteria/controller/CadastroController.java
+++ loteria/src/main/java/com/jogo/loteria/controller/CadastroController.java
@@ -1,19 +1,16 @@
 package com.jogo.loteria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-
 import com.jogo.loteria.Repository.UsuarioRepository;
 import com.jogo.loteria.model.Usuario;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -29,14 +26,19 @@ public class CadastroController {
         modelAndView.setViewName("register.html");
         return modelAndView;
     }      
-            
+
     @PostMapping("/salvarUsuario")
-    public ModelAndView salvarUsuario(@Valid Usuario usuario, BindingResult bindingResult, HttpServletRequest request) {
-        if(bindingResult.hasErrors())
-            return new ModelAndView("redirect:/cadastro");
+    public ModelAndView salvarUsuario(@ModelAttribute("usuario") @Valid Usuario usuario, BindingResult bindingResult) {
+        ModelAndView mv = new ModelAndView();
+        //adcionar comparação de senha e os erros
+        if(bindingResult.hasErrors()){
+            mv.setViewName("redirect:/cadastro");
+            mv.addObject(usuario);
+            return mv;
+        }
+        mv.setViewName("redirect:/home");
         usuarioRepository.save(usuario);
-        request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.FOUND);
-        return new ModelAndView("redirect:/home");
+        return mv;
     }
         
 }
