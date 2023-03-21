06b74da71b6950ef894c549f6274026d88485352
C-algaro <ti.chrystian@gmail.com>
2023-03-21 00:06:46 -0300

    Ajuste - Topbar

--- loteria/src/main/java/com/jogo/loteria/controller/HomeController.java
+++ loteria/src/main/java/com/jogo/loteria/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping()
